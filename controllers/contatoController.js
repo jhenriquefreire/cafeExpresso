159153f8ae46d84a07a1a2d66575cddd591fd3a3
jhenriquefreire <jh.henriquefreire@gmail.com>
2022-01-31 20:55:29 -0300

    atualização contato

---
+++ controllers/contatoController.js
@@ -0,0 +1,5 @@
+const contato = {
+    index: (req, res) => res.render('contato')
+}
+
+module.exports = contato
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const contato = require('../controllers/contatoController')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/contato', contato.index)
 
 module.exports = router;
